5e8c94a4c22bf2a136792ba99041c947f9a9a6b6
Jonathan Jayet <jonathan@artus.io>
2017-03-29 13:16:52 +0200

    Added defaultFilePath based on @fbaiodias's work

--- test/directory.js
+++ test/directory.js
@@ -890,5 +890,30 @@ describe('directory', () => {
                 done();
             });
         });
+
+        it('returns the specified default file when requesting a non existent subpath', (done) => {
+
+            const server = provisionServer();
+            server.route({ method: 'GET', path: '/directory/{path*}', handler: { directory: { path: './', defaultFilePath: 'directory/index.html' } } });
+
+            server.inject('/directory/directory/some-path', (res) => {
+
+                expect(res.statusCode).to.equal(200);
+                expect(res.payload).to.contain('test');
+                done();
+            });
+        });
+
+        it('returns 404 if the specified default file does not exist when requesting a non existent subpath', (done) => {
+
+            const server = provisionServer();
+            server.route({ method: 'GET', path: '/directory/{path*}', handler: { directory: { path: './', defaultFilePath: 'directory/invalid.html' } } });
+
+            server.inject('/directory/directory/some-path', (res) => {
+
+                expect(res.statusCode).to.equal(404);
+                done();
+            });
+        });
     });
 });
